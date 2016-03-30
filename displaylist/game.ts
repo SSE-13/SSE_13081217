ad81fc2144d29a5c7aa7bc479ef5ed28658ba8ce
120100733 <568896188@qq.com>
2016-03-30 17:55:26 +0800

    123

--- displaylist/game.ts
+++ displaylist/game.ts
@@ -11,6 +11,8 @@ var right_leg = new render.Bitmap();
 var left_arm = new render.Bitmap();
 var right_arm = new render.Bitmap();
 
+var human = new render.DisplayObjectContainer();
+
 head.source = "head.png";
 trunk.source = "trunk.png";
 left_leg.source = "left_leg.png";
@@ -18,7 +20,14 @@ right_leg.source = "right_leg.png";
 left_arm.source = "left_arm.png";
 right_arm.source = "right_arm.png";
 
-humanContainer.addChild(head)
+humanContainer.addChild(human)
+human.addChild(head)
+human.addChild(trunk)
+human.addChild(left_leg)
+human.addChild(right_leg)
+human.addChild(left_arm)
+human.addChild(right_arm)
+
 
 var renderCore = new render.RenderCore();
 renderCore.start(humanContainer, ["wander-icon.jpg"]);
